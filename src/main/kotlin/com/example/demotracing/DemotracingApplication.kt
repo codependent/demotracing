edae0ea8272784e06683b44e05189e8f3b6df627
jainigo <joseantonio.inigo@gmail.com>
2022-11-28 15:06:39 +0100

    update

--- src/main/kotlin/com/example/demotracing/DemotracingApplication.kt
+++ src/main/kotlin/com/example/demotracing/DemotracingApplication.kt
@@ -10,24 +10,13 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @SpringBootApplication
-class DemotracingApplication (private val tracer: BraveTracer){
+class DemotracingApplication(private val tracer: BraveTracer) {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @GetMapping("/hello")
     fun hello(): String {
         val currentSpan: Span? = tracer.currentSpan()
-        val newSpan: Span = tracer.nextSpan().name("hey").start()
-        try {
-            tracer.withSpan(newSpan.start()).use { ws ->
-                // You can tag a span - put a key value pair on it for better debugging
-                newSpan.tag("taxValue", "hi!")
-                // You can log an event on a span - an event is an annotated timestamp
-                newSpan.event("taxCalculated")
-            }
-        } finally {
-            newSpan.end()
-        }
         logger.info("Hello!")
         return "hello"
     }
