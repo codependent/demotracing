bdf073cc55188d2fca88d4d37248f3cf02df201e
jainigo <joseantonio.inigo@gmail.com>
2022-11-29 17:50:54 +0100

    Injected WebClientBuilder

--- src/main/kotlin/com/example/demotracing/WebClientConfiguration.kt
+++ src/main/kotlin/com/example/demotracing/WebClientConfiguration.kt
@@ -1,7 +1,5 @@
 package com.example.demotracing
 
-import io.micrometer.observation.ObservationRegistry
-import io.micrometer.observation.aop.ObservedAspect
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
