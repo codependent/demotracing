38f80b42191372e3eca048928ff02cb8ceb4c6d4
jainigo <joseantonio.inigo@gmail.com>
2022-11-30 12:47:55 +0100

    coroutines

--- src/main/kotlin/com/example/demotracing/DemotracingApplication.kt
+++ src/main/kotlin/com/example/demotracing/DemotracingApplication.kt
@@ -1,53 +1,53 @@
 package com.example.demotracing
 
 import io.micrometer.context.ContextSnapshot
+import io.micrometer.core.instrument.kotlin.asContextElement
+import io.micrometer.observation.ObservationRegistry
 import io.micrometer.observation.contextpropagation.ObservationThreadLocalAccessor
 import io.micrometer.tracing.Tracer
+import kotlinx.coroutines.reactive.awaitSingle
+import kotlinx.coroutines.withContext
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.client.WebClient
-import reactor.core.publisher.Mono
-import reactor.core.publisher.SynchronousSink
-import reactor.util.context.ContextView
+
 
 @RestController
 @SpringBootApplication
 class DemotracingApplication(
     private val webClient: WebClient,
-    private val tracer: Tracer
+    private val tracer: Tracer,
+    private val observationRegistry: ObservationRegistry
 ) {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @GetMapping("/hello")
-    fun hello(): Mono<String> {
-        return Mono.deferContextual { contextView: ContextView ->
-            ContextSnapshot.setThreadLocalsFrom(contextView, ObservationThreadLocalAccessor.KEY)
-                .use { scope: ContextSnapshot.Scope ->
-                    val traceId = tracer.currentSpan()!!.context().traceId()
-                    logger.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello!", traceId)
-                    webClient.get().uri("http://localhost:7654/helloWc")
-                        .retrieve()
-                        .bodyToMono(String::class.java)
-                        .handle { t: String, u: SynchronousSink<String> ->
-                            logger.info("Retrieved helloWc {}", t)
-                        }
-                }
+    suspend fun hello(): String {
+        val context = observationRegistry.asContextElement()
+        return withContext(context) {
+            val traceId = tracer.currentSpan()?.context()?.traceId()
+            logger.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello!", traceId)
+            val response: String = webClient.get().uri("http://localhost:7654/helloWc")
+                .retrieve()
+                .bodyToMono(String::class.java)
+                .awaitSingle()
+            response
         }
+
+
     }
 
     @GetMapping("/helloWc")
-    fun helloWc(): Mono<String> {
-        return Mono.deferContextual { contextView: ContextView ->
-            ContextSnapshot.setThreadLocalsFrom(contextView, ObservationThreadLocalAccessor.KEY)
-                .use { scope: ContextSnapshot.Scope ->
-                    val traceId = tracer.currentSpan()!!.context().traceId()
-                    logger.info("<ACCEPTANCE_TEST> <TRACE:{}> HelloWc", traceId)
-                    Mono.just(traceId)
-                }
+    suspend fun helloWc(): String {
+        val context = observationRegistry.asContextElement()
+        return withContext(context) {
+            val traceId = tracer.currentSpan()?.context()?.traceId()
+            logger.info("<ACCEPTANCE_TEST> <TRACE:{}> HelloWc", traceId)
+            "Hi there"
         }
     }
 
